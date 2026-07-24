300609851c8ae7d45e514c50b6eefa2ed456afd5
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 11:33:00 -0400

    idade minima real ag

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ if nome.lower() in ["luis", "felipe", "luis felipe", "luis felipe saff"]:
     print("Vaza irmão ngm chamou o cê aq n")
     sys.exit()
 
-idade = input("Pdp, e qnts anos ce tem lek? \n")
+idade = int(input("Pdp, e qnts anos ce tem lek? \n"))
 
-if idade <= 13:
+if idade < 13:
     print("Slk mt novo pra usar isso aqui vaza")
     sys.exit()
-    
+
 num1 = float(input("Digita um número ae prç:\n"))
 num2 = float(input("Digita outro número ae prç:\n"))
 
