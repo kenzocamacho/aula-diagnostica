4f8b6358cade030d9975171cc2339f9b9f4b9dc0
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 10:49:03 -0400

    acho q n funcionou ainda, esqueci como usa python

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ idade = input("Pdp, e qnts anos ce tem lek? \n")
 num1 = float(input("Digita um número ae prç:\n"))
 num2 = float(input("Digita outro número ae prç:\n"))
 
+while (nome == "Luis" or "LUIS" or "luis" or "FELIPE" or "felipe" or "Felipe"):
+    print("Vaza irmão, ngm chamou o cê aq n")
+    break
