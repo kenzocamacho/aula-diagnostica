c8464a45bdb5947f1c1376fecfcf1e44a30ee5f7
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 10:57:43 -0400

    agr funcionou, peguei a visao

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 
 nome = input("Qual seu nome malandro? \n")
-idade = input("Pdp, e qnts anos ce tem lek? \n")
-num1 = float(input("Digita um número ae prç:\n"))
-num2 = float(input("Digita outro número ae prç:\n"))
 
 if nome.lower() in ["luis", "felipe", "luis felipe", "luis felipe saff"]:
     print("Vaza irmão ngm chamou o cê aq n")
     sys.exit()
+
+idade = input("Pdp, e qnts anos ce tem lek? \n")
+num1 = float(input("Digita um número ae prç:\n"))
+num2 = float(input("Digita outro número ae prç:\n"))
+
+
+
