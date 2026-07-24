4fa83bc2ca49c3bec794d37549806e702e7894c9
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 11:00:47 -0400

    agr tem idade minima pae

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ if nome.lower() in ["luis", "felipe", "luis felipe", "luis felipe saff"]:
     sys.exit()
 
 idade = input("Pdp, e qnts anos ce tem lek? \n")
+
+if idade <= 13:
+    print("Slk mt novo pra usar isso aqui vaza")
+    sys.exit()
+    
 num1 = float(input("Digita um número ae prç:\n"))
 num2 = float(input("Digita outro número ae prç:\n"))
 
