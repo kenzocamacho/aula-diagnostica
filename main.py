07da95f48eca394544b78d6139d7452cd6b98e14
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 11:40:39 -0400

    lógica ai

--- main.py
+++ main.py
@@ -21,4 +21,25 @@ print("[2] Subtrair (-)")
 print("[3] Multiplicar (*)")
 print("[4] Dividir (/)")
 
-opcao = input("Digite o número da operação: ")
+opcao = input("Digite ai q numero ce qr: ")
+
+print("\n" + "="*30)
+if opcao == "1":
+    resultado = num1 + num2
+    print(f"Resultado da Soma: {resultado}")
+elif opcao == "2":
+    resultado = num1 - num2
+    print(f"Resultado da Subtração: {resultado}")
+elif opcao == "3":
+    resultado = num1 * num2
+    print(f"Resultado da Multiplicação: {resultado}")
+elif opcao == "4":
+    # Evita que o programa quebre se o usuário tentar dividir por zero
+    if num2 == 0:
+        print("Malandro, não dá pra dividir por zero né")
+    else:
+        resultado = num1 / num2
+        print(f"Resultado da Divisão: {resultado}")
+else:
+    print("Ce é cego, mlk? Escolhe de 1 a 4, cara. Faz favor fia")
+print("="*30)
