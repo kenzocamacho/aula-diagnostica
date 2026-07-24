8b09c5aac069622377206497ea757b014cfbc1c8
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 10:41:39 -0400

    mais variavel q ce vai vr pra q

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+nome = input("Qual seu nome malandro? \n")
+idade = input("Pdp, e qnts anos ce tem lek? \n")
+
 num1 = float(input("Digita um número ae prç:\n"))
 num2 = float(input("Digita outro número ae prç:\n"))
 
