a948e1e198600079bac174b6dbdb86be17a9ea5f
Kenzo Raul Pimenta Camacho - SENAI - Cuiaba/MT <00622609@alunosenai.mt>
2026-07-24 11:38:09 -0400

    escolhas

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ if nome.lower() in ["luis", "felipe", "luis felipe", "luis felipe saff"]:
 
 idade = int(input("Pdp, e qnts anos ce tem lek? \n"))
 
-if idade < 13:
+if idade <= 13:
     print("Slk mt novo pra usar isso aqui vaza")
     sys.exit()
 
 num1 = float(input("Digita um número ae prç:\n"))
 num2 = float(input("Digita outro número ae prç:\n"))
 
+print("\nEscolha a operação prç:")
+print("[1] Somar (+)")
+print("[2] Subtrair (-)")
+print("[3] Multiplicar (*)")
+print("[4] Dividir (/)")
 
-
+opcao = input("Digite o número da operação: ")
